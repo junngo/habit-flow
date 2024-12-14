21dc667edf394ed8502072418820a9990454ca21
junngo <myeongjun.ko@gmail.com>
2024-12-14 22:49:54 +0900

    added the docker-compose to run the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'habitflow_user',
+      password: 'habitflow_password',
+      database: 'habitflow_db',
+      entities: [],
+      synchronize: true, // 개발 환경에서만 사용. 운영 환경에서는 false로 설정.
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
