e2a603fc9866bd0a7568b71fd20477321f33a171
junngo <myeongjun.ko@gmail.com>
2024-12-16 09:44:45 +0900

    Sign up mock test for unittest

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -10,8 +10,10 @@ describe('AuthService', () => {
   let authService: AuthService;
   let usersService: Partial<UsersService>;
 
-  const mockUsersRepository = {
-    findOne: jest.fn(), // UsersRepository.findOne 메서드 Mock
+  const usersRepository = {
+    findOne: jest.fn(), // UsersRepository.findOne 메서드
+    save: jest.fn(),
+    create: jest.fn(),
   };
 
   const mockJwtService = {
@@ -25,7 +27,7 @@ describe('AuthService', () => {
         AuthService,
         {
           provide: getRepositoryToken(Users), // UsersRepository 토큰 제공
-          useValue: mockUsersRepository,
+          useValue: usersRepository,
         },
         {
           provide: UsersService,
@@ -47,5 +49,59 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(authService).toBeDefined();
+  });
+
+  it('should create a new user with hashed password', async () => {
+    const userDto = {
+      email: 'test@example.com',
+      username: 'TestUser',
+      password: 'UserPlainPassword',
+    };
+
+    // Mock: 중복 이메일 확인
+    usersRepository.findOne.mockResolvedValue(null);
+    usersRepository.create.mockReturnValue({
+      email: userDto.email,
+      username: userDto.username,
+      password: 'UserHashedPassword',
+    });
+    usersRepository.save.mockResolvedValue({
+      id: 1,
+      email: userDto.email,
+      username: userDto.username,
+      password: 'UserHashedPassword',
+      walletAddress: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    // // Mock: 비밀번호 해시화
+    jest.spyOn(bcrypt, 'hash').mockImplementation(async () => 'UserHashedPassword');
+
+    // // 테스트 실행
+    const user = await authService.signup(userDto);
+
+    // 검증
+    expect(usersRepository.findOne).toHaveBeenCalledWith({ where: { email: userDto.email } });
+    expect(bcrypt.hash).toHaveBeenCalledWith(userDto.password, 10);
+    expect(usersRepository.create).toHaveBeenCalledWith({
+      email: userDto.email,
+      username: userDto.username,
+      password: 'UserHashedPassword',
+    });
+    expect(usersRepository.save).toHaveBeenCalledWith({
+      email: userDto.email,
+      username: userDto.username,
+      password: 'UserHashedPassword',
+    });
+    expect(user).toEqual({
+      id: 1,
+      email: userDto.email,
+      username: userDto.username,
+      password: 'UserHashedPassword',
+      walletAddress: null,
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
   });
 })
