f2836036fc1f466f3ddea48abd7c278ea7893506
junngo <myeongjun.ko@gmail.com>
2024-12-26 10:14:43 +0900

    creating the frontend and added the new api to verify the token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { FirebaseAuthGuard } from './guards/firebase-auth.guard';
 import { Request } from 'express';
+import admin from "./firebase-admin"
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,24 @@ export class AuthController {
   async signup(@Req() req: Request) {
     const { email, username, userId: external_uid } = req.user!;
     return this.authService.validateOrCreateUser(email, username, external_uid);
+  }
+
+  @UseGuards(FirebaseAuthGuard)
+  @Get('verify-token')
+  async verifyToken(@Req() req: Request) {
+    const { userId: external_uid } = req.user!;
+
+    try {
+      const customToken = await admin.auth().createCustomToken(external_uid);
+      console.log("customToken: ", customToken);
+      return {
+        status: 'success',
+        message: 'Token is valid',
+        user: req.user,
+        customToken: customToken
+      };
+    } catch (error) {
+      console.error('Firebase token verification failed:', error.message);
+    }
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS 설정: localhost:3000에서의 요청을 허용
+  app.enableCors({
+    origin: 'http://localhost:3000', // CORS 허용할 도메인
+    methods: 'GET,POST,PUT,DELETE',  // 허용할 HTTP 메소드
+    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더
+  });
   app.useGlobalPipes(new ValidationPipe());         // DTO 유효성 검사 활성화
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
